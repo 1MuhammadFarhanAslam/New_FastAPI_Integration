75fa0d1988adc4c4abd6afbeccd974e1c48f030a
isharab <sherijigarx@gmail.com>
2024-04-21 20:44:16 +0500

    Update text_to_music.py

--- models/text_to_music.py
+++ models/text_to_music.py
@@ -39,12 +39,12 @@ class MusicGenerator:
         self.model = MusicGen.get_pretrained(self.model_name)
         # self.sample_rate = self.model.sample_rate
 
-    def generate_music(self, prompt, duration=15): #, temperature=0.7, top_k=250
+    def generate_music(self, prompt, duration=15, temperature=0.7, top_k=250):
         """Generate music based on a given description."""
         self.model.set_generation_params(
             duration=duration,
-            # temperature=temperature,
-            # top_k=top_k
+            temperature=temperature,
+            top_k=top_k
         )
 
         try:
