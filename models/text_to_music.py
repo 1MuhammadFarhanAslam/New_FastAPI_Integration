eebfecb2f00d19cce284e158e030ac7aedf7e948
isharab <sherijigarx@gmail.com>
2024-04-21 18:17:48 +0500

    valid

--- models/text_to_music.py
+++ models/text_to_music.py
@@ -1,23 +1,28 @@
 import torch
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+
+
 class MusicGenerator:
     def __init__(self, model_path="facebook/musicgen-medium"):
         self.model_name = model_path  
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.processor = AutoProcessor.from_pretrained(self.model_name)
-        self.model = MusicgenForConditionalGeneration.from_pretrained(self.model_name)
+        # self.processor = AutoProcessor.from_pretrained(self.model_name)
+        # self.model = MusicgenForConditionalGeneration.from_pretrained(self.model_name)
+        self.model = MusicGen.get_pretrained(self.model_name)
         self.model.to(self.device)
 
     def generate_music(self, prompt, duration):
         self.model = self.model.set_generation_params(duration=duration)
+        text=[prompt],
         try:
-            inputs = self.processor(
-                text=[prompt],
-                padding=True,
-                return_tensors="pt",
-            ).to(self.device)
-            audio_values = self.model.generate(**inputs)
+            # inputs = self.processor(
+            #     padding=True,
+            #     return_tensors="pt",
+            # ).to(self.device)
+            audio_values = self.model.generate(text)
             return audio_values[0, 0].cpu().numpy()
         except Exception as e:
             print(f"An error occurred with {self.model_name}: {e}")
