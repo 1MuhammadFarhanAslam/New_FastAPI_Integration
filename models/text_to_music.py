fd7b2c2c1484562dcff77791cf332f11bc92e11e
isharab <sherijigarx@gmail.com>
2024-04-21 18:36:13 +0500

    duration

--- models/text_to_music.py
+++ models/text_to_music.py
@@ -37,14 +37,14 @@ class MusicGenerator:
     def __init__(self, model_path="facebook/musicgen-medium"):
         self.model_name = model_path  
         self.model = MusicGen.get_pretrained(self.model_name)
-        self.sample_rate = self.model.sample_rate
+        # self.sample_rate = self.model.sample_rate
 
-    def generate_music(self, prompt, duration=15, temperature=0.7, top_k=250):
+    def generate_music(self, prompt, duration=15): #, temperature=0.7, top_k=250
         """Generate music based on a given description."""
         self.model.set_generation_params(
             duration=duration,
-            temperature=temperature,
-            top_k=top_k
+            # temperature=temperature,
+            # top_k=top_k
         )
 
         try:
